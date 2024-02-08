2a365f7c3804f31ee1e0fe562cfecb16d2bf936d
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-02-08 08:57:11 -0300

    classes anônimas

---
+++ src/br/com/devdojo/maratonajava/Eclassesinternas/test/OuterClassesTest01.java
@@ -0,0 +1,11 @@
+package br.com.devdojo.maratonajava.Eclassesinternas.test;
+
+public class OuterClassesTest01 {
+    private String nome = "Sasuke";
+
+    class InnerClass
+    public static void main(String[] args) {
+        //classe interna: consiste em uma classe dentro de outra classe
+
+    }
+}
